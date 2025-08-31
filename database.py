88a104e22dd778e5141c72f272b19d812e1fa0bd
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-08-31 21:02:56 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+# backend/database.py
+from supabase import create_client
+import os
+
+supabase = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_SERVICE_ROLE_KEY")  # Admin access
+)
