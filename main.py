424096199853b29ff072eaa3929fdb7256751e63
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-09-01 14:58:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-exam-tutor.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://ai-exam-tutor-frontend.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
